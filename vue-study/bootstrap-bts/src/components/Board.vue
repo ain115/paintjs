9e3c5d33b628b69fe69cf2a89c601e3d6f013c9a
ain115 <c00a@naver.com>
2019-01-01 17:00:49 +0900

    .

--- vue-study/bootstrap-bts/src/components/Board.vue
+++ vue-study/bootstrap-bts/src/components/Board.vue
@@ -1,39 +1,79 @@
 <template>
   <div>
-    <b-table striped hover :items="items" :fields="fields"></b-table>
+    <b-table 
+      striped hover 
+      :per-page="perPage"
+      :current-page="currentPage" 
+      :items="items" 
+      :fields="fields" 
+      @row-clicked="rowClick"></b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      align="center"
+    ></b-pagination>
+    <b-button @click="writeContent">글쓰기</b-button>
   </div>
 </template>
 
 <script>
-import data from '@/data'
-//글번호 내림차순으로 정렬
-let items = data.Content.sort((a,b) => {return b.content_id - a.content_id})
-//data에 있는 값을 모든 값을 출력하게 하는 코드
-items = items.map(contentItem => {return {...contentItem, user_name: data.User.filter(userItem => userItem.user_id === contentItem.user_id)[0].name}})
+import data from "@/data";
+
 export default {
-  name: 'Board',
+  name: "Board",
   data() {
+    let items = data.Content.sort((a, b) => {
+      return b.content_id - a.content_id;
+    });
+    items = items.map(contentItem => {
+      return {
+        ...contentItem,
+        user_name: data.User.filter(
+          userItem => userItem.user_id === contentItem.user_id
+        )[0].name
+      };
+    });
     return {
+      currentPage: 1,
+      perPage: 10,
       fields: [
         {
-          key: 'content_id',
-          label: '글번호',
+          key: "content_id",
+          label: "글번호"
         },
         {
-          key: 'title',
-          label: '제목',
+          key: "title",
+          label: "제목"
         },
         {
-          key: 'created_at',
-          label: '등록일'
+          key: "created_at",
+          label: "등록일"
         },
         {
-          key: 'user_name',
-          label: '글쓴이'
-        },
+          key: "user_name",
+          label: "글쓴이"
+        }
       ],
-      items
+      items: items
+    };
+  },
+  computed: {
+    rows() {
+      return this.items.length;
+    }
+  },
+  methods: {
+    rowClick(item, index, e) {
+      this.$router.push({
+        path: `/board/free/detail/${item.content_id}`
+      });
+    },
+    writeContent() {
+      this.$router.push({
+        path: "/board/free/create"
+      });
     }
   }
-}
+};
 </script>
