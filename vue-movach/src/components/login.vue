f9e99d742a22dd3450fa8dee611c23fc210ba98b
ain115 <c00a@naver.com>
2020-06-03 00:25:38 +0900

    충돌 수정

--- vue-movach/src/components/login.vue
+++ vue-movach/src/components/login.vue
@@ -1,15 +1,10 @@
 <template>
     <div>
-<<<<<<< HEAD
-        <input type="text" placeholder="ID 입력해주세요" />
-        <input type="password" placeholder="비밀번호를 입력해주세요" />
-=======
         <form>
             <input type="text" placeholder="아이디를 입력해주세요" />
             <input type="password" placeholder="비밀번호를 입력해주세요" />
             <button>로그인</button>
         </form>
->>>>>>> 1ad8426882d4a980eb61cbd5e9c3014cd0a43fa9
     </div>
 </template>
 <script>
