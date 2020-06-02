2321d36af852238ef0f4c1f280ff37d9294e9f03
ain115 <c00a@naver.com>
2020-06-02 17:50:10 +0900

    no message

--- vue-movach/src/components/login.vue
+++ vue-movach/src/components/login.vue
@@ -1,5 +1,8 @@
 <template>
-    <div></div>
+    <div>
+        <input type="text" placeholder="ID 입력해주세요" />
+        <input type="password" placeholder="비밀번호를 입력해주세요" />
+    </div>
 </template>
 <script>
 export default {
